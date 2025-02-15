d10ee17d016c7536bddfb500f7ea9a672a5b004f
MLD <dissanayaka.lakmal@gmail.com>
2025-02-15 10:35:59 +0530

    feat: Create second module :sparkles:

--- src/Lambda_function.py
+++ src/Lambda_function.py
@@ -1,8 +1,9 @@
 import json
+from src.extra_utils import get_lambda_message
 
 def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! - Update V2')
+        'body': json.dumps(get_lambda_message())
     }
